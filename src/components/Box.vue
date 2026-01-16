a51e5fef2b5b4b36608a65f7ad968abb11b39702
Werliton Silva <werliton.silva@osf.digital>
2026-01-16 09:38:37 -0300

    feat: add new way to style component

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -3,16 +3,17 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "BoxComponent",
+  data() {
+    return {
+      estilos: {
+        "background-color": "#faf0ca",
+      },
+    };
+  },
 });
 </script>
 <template>
-  <div class="box has-text-weight-bold">
+  <div class="box has-text-weight-bold" :style="estilos">
     <slot></slot>
   </div>
 </template>
-
-<style scoped>
-.box {
-  background-color: #fafaf0;
-}
-</style>
